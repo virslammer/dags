c834a3ce25f1aa655d032d6b4098492f7ec4930f
virslammer <virslammer@gmail.com>
2021-06-30 14:49:01 +0700

    Update utils lib into file

--- las_file_ingestor.py
+++ las_file_ingestor.py
@@ -9,11 +9,81 @@ from minio import Minio
 from io import BytesIO
 from dag_util.minio_client import minio_client
 from dag_util.dremio_client import login_dremio, add_tag_for_catalog, promote_file_to_dataset
-from dag_util.catalog_service_client import FILE_CATALOG_SERVICE_URL
 import pandas as pd
 import os
 import json
 import requests
+import urllib.parse
+
+# SETTING  catalog_service , minio and dremio 
+FILE_CATALOG_SERVICE_URL =  r"http://10.88.231.37:31624/api/v1/catalog/file/"
+# dremio base url
+DREMIO_BASE_URL = "http://10.88.231.37:31220/api/v3/catalog/"
+DREMIO_LOGIN_URL = "http://10.88.231.37:31220/apiv2/login"
+# dremio credential
+USERNAME = "admin"
+PASSWORD = "admin1234"
+HEADERS = {
+    'Content-Type': "application/json"
+}
+# dremio file default path
+DREMIO_DEFAULT_PATH = "minio-source/datalake/landing_zone"
+
+
+def login_dremio ():
+    payload = {"userName": USERNAME, "password": PASSWORD}
+    response = requests.request("POST", DREMIO_LOGIN_URL, data=json.dumps(payload), headers=HEADERS)
+    response_data = json.loads(response.text)
+    return response_data['token']
+def promote_file_to_dataset (filename, token):
+    HEADERS['Authorization'] = "_dremio"+token
+    catalog_by_path_endpoint = DREMIO_BASE_URL + r"by-path/" + DREMIO_DEFAULT_PATH + "/" + filename
+    check_file_on_dremio = requests.request("GET", catalog_by_path_endpoint,headers=HEADERS )
+    # id = "dremio:/" + DREMIO_DEFAULT_PATH + "/" + filename
+    check_file_on_dremio_info = json.loads(check_file_on_dremio.text)
+    id = check_file_on_dremio_info['id']
+    path = check_file_on_dremio_info['path']
+    encode_id = urllib.parse.quote_plus(id)
+    # path = DREMIO_DEFAULT_PATH.split("/")
+    # path.append("/" +filename)
+    # path = os.path.join(DREMIO_DEFAULT_PATH,filename).split("/")
+    if check_file_on_dremio_info['entityType'] != 'dataset':
+        payload = {
+            "entityType": "dataset",
+            "type": "PHYSICAL_DATASET",
+            "id": encode_id,
+            "path": path,
+            "format": {
+                "type": "Parquet"
+            }
+        }
+        response = requests.request("POST", DREMIO_BASE_URL + encode_id, data=json.dumps(payload), headers=HEADERS)
+        response_data = json.loads(response.text)
+        return response_data['id']
+    else:
+        # Todo : update Dataset  - Now: return the id of file
+        return id
+def add_tag_for_catalog (catalog_id, token, tags):
+    payload = {
+        "tags": tags
+    }
+    HEADERS['Authorization'] = "_dremio"+token
+    response = requests.request("POST", DREMIO_BASE_URL + catalog_id + "/collaboration/tag", data=json.dumps(payload), headers=HEADERS)
+    response_data = json.loads(response.text)
+    return response_data
+    
+MINIO_URL =  '10.88.231.36:9000'
+MINIO_ACCESS_KEY =  'minioadmin'
+MINIO_SECRET_KEY =  'minioadmin'
+minio_client = Minio(
+    MINIO_URL,
+    access_key=MINIO_ACCESS_KEY,
+    secret_key=MINIO_SECRET_KEY, 
+    secure = False
+)
+
+
+# --------------------------------------------------
 default_args = {
     'owner': 'dmp',
     'depends_on_past': False,
